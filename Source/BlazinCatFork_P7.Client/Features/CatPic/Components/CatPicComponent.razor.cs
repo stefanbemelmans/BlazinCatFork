2fa1c535b148ade0304635e9d5dcb39fd186d28b
Stefan L. Bemelmans <stefanlbemelmans@gmail.com>
2019-08-18 03:54:06 -0500

    CatFork Rides!

---
+++ Source/BlazinCatFork_P7.Client/Features/CatPic/Components/CatPicComponent.razor.cs
@@ -0,0 +1,20 @@
+﻿using BlazinCatFork_P7.Client.Features.Base.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazinCatFork_P7.Client.Features.CatPic.Components
+{
+    public class CatPicComponentModel : BaseComponent
+    {
+    public string CatPicUrl => CatPicState.CatPicUrl;
+
+    public async Task NewCat() => await Mediator.Send(new GetCatPicAction());
+
+    protected override async Task OnInitAsync() => await NewCat();
+  }
+
+
+}
