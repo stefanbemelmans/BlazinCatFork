1aba8154f03fba245411b641827e2158e0c86e35
Stefan L. Bemelmans <stefanlbemelmans@gmail.com>
2019-08-23 13:03:38 -0500

    Recipe Search Result Component

---
+++ Source/BlazinCatFork_P8.Client/Features/Recipes/Components/RecipeSearchResultComponent/RecipeSearchResultComponentBase.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazinCatFork_P8.Client.Features.Recipes.Components.RecipeSearchResultComponent
+{
+  using BlazinCatFork_P8.Client.Features.Base.Components;
+  using Microsoft.AspNetCore.Components;
+  using System;
+  using System.Threading.Tasks;
+
+  public class RecipeSearchResultComponentBase : BaseComponent
+  {
+    [Parameter]
+    public RecipeSearchResult RecipeSearchResult { get; set; }
+    public void GetRecipeDetails()
+    {
+      // Call the GetRecipeDetails api endpoint
+
+      Console.WriteLine($"Recipe ID: {RecipeSearchResult.id}");
+    }
+  }
+
+}
