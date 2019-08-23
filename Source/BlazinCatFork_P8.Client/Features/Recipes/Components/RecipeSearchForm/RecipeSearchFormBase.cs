f82fa97cf03ea3fae1f71c5cd13ddf6f26f45d68
Stefan L. Bemelmans <stefanlbemelmans@gmail.com>
2019-08-23 01:43:16 -0500

    Form Data moving

--- Source/BlazinCatFork_P8.Client/Features/Recipes/Components/RecipeSearchForm/RecipeSearchFormBase.cs
+++ Source/BlazinCatFork_P8.Client/Features/Recipes/Components/RecipeSearchForm/RecipeSearchFormBase.cs
@@ -3,16 +3,19 @@
   using BlazinCatFork_P8.Client.Features.Base.Components;
   using BlazinCatFork_P8.Shared.Features.SpoonacularApi;
   using System;
-
+  
   public class RecipeSearchFormBase : BaseComponent
   {
 
-    public SharedRecipeSearchRequest RecipeSearchFormInputs { get; set; }
+    public SharedRecipeSearchRequest RecipeSearchFormInputs { get; set; } = new SharedRecipeSearchRequest() 
+    {
+      ingredients = "chicken, onions"
+    };
    
     
     public void SumbitValidForm()
     {
-
+      Console.WriteLine("Form Submitted Successfully!");
       Console.WriteLine($"{RecipeSearchFormInputs.ignorePantry}");
       Console.WriteLine($"{RecipeSearchFormInputs.ranking}");
       Console.WriteLine($"{RecipeSearchFormInputs.number}");
